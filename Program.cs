12d382f490922f0b53f0261346144564e17abe4e
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-08-31 18:23:08 +0200

    cast overflow

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
-﻿int num1 = 300;
-byte num2 = (byte)num1;
+﻿int num1 = 255;
+byte num2 ;
+
+for (int i = 0; i < 10; i++)
+{
+    num1++;
+    num2 = (byte)num1;
+    
 Console.WriteLine("{0}, {1}",num1, num2 );
+}
