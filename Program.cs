0befcad20317e5112837edfdf91be98295c49ec1
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-09-01 17:46:01 +0200

    ex2.1 div by 3

--- Program.cs
+++ Program.cs
@@ -4,28 +4,12 @@
 
         static void Main(string[] args)
         {
-            byte speed_limit;
-            byte vehicle_speed;
-            byte accumulated_points;
-            System.Console.WriteLine("please enter the speed limit");
-            speed_limit = Convert.ToByte(System.Console.ReadLine());
-            System.Console.WriteLine("please enter the vehicle speed");
-            vehicle_speed = Convert.ToByte(System.Console.ReadLine());
-
-            if (speed_limit > vehicle_speed)
+            for (int i = 1; i <= 100; i++)
             {
-                System.Console.WriteLine("not speeding");
-            }
-            else
-            {
-                accumulated_points = (byte)((vehicle_speed - speed_limit) / 5);
-                if (accumulated_points < 12)
+                if (i % 3 == 0)
                 {
-                    System.Console.WriteLine("you have accumulated {0} points", accumulated_points);
-                }else
-                {
-                    System.Console.WriteLine("your license has been suspended due to having 60accumulated {0} points", accumulated_points);
-                }          
+                    System.Console.WriteLine("{0} is divisible by 3", i);
+                }
             }
         }
     }
