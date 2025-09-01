30dd0ab7faa565a0c0a1fc3b926942e170de7404
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-09-01 22:06:40 +0200

    taking a break

--- Program.cs
+++ Program.cs
@@ -5,23 +5,11 @@
         static void Main(string[] args)
         {
 
-           int[] numlist = new int[5] { 0,0,0,0,0};
-            for (int i = 0; i < 5; i++)
-            {
-                int temp;
-                System.Console.WriteLine("enter a number: ");
-                temp = Convert.ToInt32(System.Console.ReadLine());
-                while (numlist.Contains(temp))
-                {
-                    System.Console.WriteLine("please try a different number");
-                    temp = Convert.ToInt32(System.Console.ReadLine());
-                }
-                numlist[i] = temp;
-            }
-            foreach (var item in numlist)
-            {
-                System.Console.WriteLine(item);
-            }
+            String input_time;
+            System.Console.WriteLine("please enter a time ");
+            input_time = System.Console.ReadLine(); 
+
+            // if(new DateTime)// TODO:  Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.
 
         }
     }
