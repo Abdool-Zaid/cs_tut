57e3ff07b1c086c77ecc99f08f2c1bdda00c4618
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-09-01 18:52:30 +0200

    ex3.2 reverse string

--- Program.cs
+++ Program.cs
@@ -4,35 +4,14 @@
 
         static void Main(string[] args)
         {
-            var liked_by = new List<string>();
-            while (true)
-            {
-                System.Console.WriteLine("enter a name");
-                var name = System.Console.ReadLine();
-                if (String.IsNullOrWhiteSpace(name))
-                {
-                    break;
-                }
-                else
-                {
-                    liked_by.Add(name);
-                    if (liked_by.Count == 1)
-                    {
-                        System.Console.WriteLine("{0} liked your post", name);
-                    }
-                    else if (liked_by.Count == 2)
-                    {
-                        System.Console.WriteLine("your post was liked by {0} and {1}", liked_by[0], liked_by[1]);
-                    }
-                    else if (liked_by.Count > 2)
-                    {
-                        System.Console.WriteLine("your post was liked by {0},{1} and {2} more ", liked_by[0], liked_by[1], (liked_by.Count-2));
-                    }
+            String name;
+            System.Console.WriteLine("please enter a name");
+            name = System.Console.ReadLine();
+            char[] charArray = name.ToCharArray();
+            Array.Reverse(charArray);
+            var rev_name = new String(charArray);
 
-
-                }
-
-            }
+            System.Console.WriteLine(rev_name);
 
         }
     }
