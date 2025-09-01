cdcce658162e62c0dce92265dd232040979da82d
Abdool-Zaid <aztoorabally7447@gmail.com>
2025-09-01 14:02:13 +0200

    basic calculator

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
-﻿int num1 = 255;
-byte num2 ;
-
-for (int i = 0; i < 10; i++)
+﻿namespace Cs_tut
 {
-    num1++;
-    num2 = (byte)num1;
+    public class Calculator
+    {
+        public int add(int a, int b) {
+            return (a + b);
+        }
+         public int subtract(int a, int b) {
+            return (a - b);
+        }
+        public int mul(int a, int b) {
+            return (a * b);
+        }
+        public int div(int a, int b) {
+            return (a / b);
+        }
+
+
+    }
+    public class Program{
+
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+                System.Console.WriteLine(calc.add(9,10));
+                System.Console.WriteLine(calc.subtract(9,10));
+                System.Console.WriteLine(calc.mul(9,10));
+                System.Console.WriteLine(calc.div(9,10));
+
+        }
+    }
     
-Console.WriteLine("{0}, {1}",num1, num2 );
 }
